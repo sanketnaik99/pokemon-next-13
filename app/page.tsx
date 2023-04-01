1fa2ff68fab9d79ddba71d60cc89f6903c04802d
Sanket Naik <sanketnaik99@gmail.com>
2023-04-01 00:17:52 -0700

    Adds redirect for home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import { Inter } from "next/font/google";
+import { redirect } from "next/navigation";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default async function Home() {
-  return <section className="body-font">This is the Root page</section>;
+  redirect("/home");
 }
